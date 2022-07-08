8e20129630213375dfd4bfd5c2c07195d0621349
nico <nicoparulian2@gmail.com>
2022-07-08 23:17:39 +0700

    docs api

--- routes/index.route.js
+++ routes/index.route.js
@@ -4,13 +4,17 @@ const router = express.Router();
 const authRoutes = require("./auth.route");
 const profileRoutes = require("./profile.route");
 const productsRoutes = require("./product.route");
-const wihslistRoutes = require("./wishlist.route")
-const notifRoutes = require("./notif.route")
+const wihslistRoutes = require("./wishlist.route");
+const notifRoutes = require("./notif.route");
+
+router.get("/", (req, res, next) => {
+  res.redirect("/api-docs");
+});
 
 router.use("/api/v1/auth", authRoutes);
 router.use("/api/v1/profile", profileRoutes);
 router.use("/api/v1/product", productsRoutes);
-router.use("/api/v1/wishlist", wihslistRoutes)
-router.use("/api/v1/notif", notifRoutes)
+router.use("/api/v1/wishlist", wihslistRoutes);
+router.use("/api/v1/notif", notifRoutes);
 
 module.exports = router;
